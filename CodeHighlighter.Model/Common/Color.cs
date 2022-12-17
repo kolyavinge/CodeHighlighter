a9ca366922b3821ef42c53649ab62cf2bbab182b
Vinge Nikolay <vn2756@gmail.com>
2022-12-17 18:02:07 +0400

    Color.FromHex

--- CodeHighlighter.Model/Common/Color.cs
+++ CodeHighlighter.Model/Common/Color.cs
@@ -2,20 +2,44 @@
 
 public readonly struct Color
 {
+    public static Color FromHex(string hex)
+    {
+        hex = hex.Trim().Replace("#", "");
+        if (hex.Length == 6)
+        {
+            var r = Convert.ToByte(hex.Substring(0, 2), 16);
+            var g = Convert.ToByte(hex.Substring(2, 2), 16);
+            var b = Convert.ToByte(hex.Substring(4, 2), 16);
+            return new(r, g, b);
+        }
+        else if (hex.Length == 8)
+        {
+            var a = Convert.ToByte(hex.Substring(0, 2), 16);
+            var r = Convert.ToByte(hex.Substring(2, 2), 16);
+            var g = Convert.ToByte(hex.Substring(4, 2), 16);
+            var b = Convert.ToByte(hex.Substring(6, 2), 16);
+            return new(a, r, g, b);
+        }
+        else
+        {
+            throw new ArgumentException("Incorrect hex value.");
+        }
+    }
+
+    public readonly byte A;
     public readonly byte R;
     public readonly byte G;
     public readonly byte B;
-    public readonly byte A;
 
-    public Color(byte r, byte g, byte b, byte a)
+    public Color(byte a, byte r, byte g, byte b)
     {
+        A = a;
         R = r;
         G = g;
         B = b;
-        A = a;
     }
 
-    public Color(byte r, byte g, byte b) : this(r, g, b, 255)
+    public Color(byte r, byte g, byte b) : this(255, r, g, b)
     {
     }
 }
---
+++ CodeHighlighter.Tests.Model/Common/ColorTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using CodeHighlighter.Common;
+using NUnit.Framework;
+
+namespace CodeHighlighter.Tests.Common;
+
+internal class ColorTest
+{
+    [Test]
+    public void FromHex_Sharp()
+    {
+        var color = Color.FromHex("#ff32a852");
+        Assert.That(color.A, Is.EqualTo(255));
+        Assert.That(color.R, Is.EqualTo(50));
+        Assert.That(color.G, Is.EqualTo(168));
+        Assert.That(color.B, Is.EqualTo(82));
+    }
+
+    [Test]
+    public void FromHex_NoSharp()
+    {
+        var color = Color.FromHex("ff32a852");
+        Assert.That(color.A, Is.EqualTo(255));
+        Assert.That(color.R, Is.EqualTo(50));
+        Assert.That(color.G, Is.EqualTo(168));
+        Assert.That(color.B, Is.EqualTo(82));
+    }
+
+    [Test]
+    public void FromHex_SharpNoAlpha()
+    {
+        var color = Color.FromHex("#32a852");
+        Assert.That(color.A, Is.EqualTo(255));
+        Assert.That(color.R, Is.EqualTo(50));
+        Assert.That(color.G, Is.EqualTo(168));
+        Assert.That(color.B, Is.EqualTo(82));
+    }
+
+    [Test]
+    public void FromHex_NoSharpNoAlpha()
+    {
+        var color = Color.FromHex("32a852");
+        Assert.That(color.A, Is.EqualTo(255));
+        Assert.That(color.R, Is.EqualTo(50));
+        Assert.That(color.G, Is.EqualTo(168));
+        Assert.That(color.B, Is.EqualTo(82));
+    }
+
+    [Test]
+    public void FromHex_Empty_Error()
+    {
+        try
+        {
+            Color.FromHex("");
+        }
+        catch (ArgumentException e)
+        {
+            Assert.That(e.Message, Is.EqualTo("Incorrect hex value."));
+        }
+    }
+
+    [Test]
+    public void FromHex_Incorrect()
+    {
+        try
+        {
+            Color.FromHex("32a85");
+        }
+        catch (ArgumentException e)
+        {
+            Assert.That(e.Message, Is.EqualTo("Incorrect hex value."));
+        }
+    }
+
+    [Test]
+    public void FromHex_Incorrect_2()
+    {
+        try
+        {
+            Color.FromHex("32a8");
+        }
+        catch (ArgumentException e)
+        {
+            Assert.That(e.Message, Is.EqualTo("Incorrect hex value."));
+        }
+    }
+}
