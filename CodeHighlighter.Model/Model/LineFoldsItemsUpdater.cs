28dd8356a32f7a3edda6efc2b8725385b1f8cf2b
Vinge Nikolay <vn2756@gmail.com>
2023-03-23 21:08:02 +0400

    LineFoldsItemsUpdater

---
+++ CodeHighlighter.Model/Model/LineFoldsItemsUpdater.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeHighlighter.Model;
+
+public interface ILineFoldsItemsUpdater
+{
+    void Update(ILineFolds folds, IEnumerable<LineFold> items);
+}
+
+public class LineFoldsItemsUpdater : ILineFoldsItemsUpdater
+{
+    public void Update(ILineFolds folds, IEnumerable<LineFold> items)
+    {
+        var newFolds = new List<LineFold>();
+        var foldsDictionary = folds.Items.ToDictionary(k => k.LineIndex, v => v);
+        var needToSet = false;
+        foreach (var item in items)
+        {
+            if (foldsDictionary.ContainsKey(item.LineIndex) && item.LinesCount == foldsDictionary[item.LineIndex].LinesCount)
+            {
+                newFolds.Add(foldsDictionary[item.LineIndex]);
+            }
+            else
+            {
+                newFolds.Add(item);
+                needToSet = true;
+            }
+        }
+        if (needToSet || folds.Items.Count != newFolds.Count)
+        {
+            folds.Items = newFolds;
+        }
+    }
+}
---
+++ CodeHighlighter.Tests.Model/Model/LineFoldsItemsUpdaterTest.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using CodeHighlighter.Model;
+using Moq;
+using NUnit.Framework;
+
+namespace CodeHighlighter.Tests.Model;
+
+internal class LineFoldsItemsUpdaterTest
+{
+    private List<LineFold> _oldItems;
+    private List<LineFold> _newItems;
+    private Mock<ILineFolds> _folds;
+    private LineFoldsItemsUpdater _updater;
+
+    [SetUp]
+    public void Setup()
+    {
+        _oldItems = new List<LineFold>();
+        _newItems = new List<LineFold>();
+        _folds = new Mock<ILineFolds>();
+        _updater = new LineFoldsItemsUpdater();
+    }
+
+    [Test]
+    public void Update_EmptyOldItems()
+    {
+        _newItems.Add(new(1, 2));
+        _newItems.Add(new(5, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = _newItems, Times.Once());
+    }
+
+    [Test]
+    public void Update_EqualItems()
+    {
+        _oldItems.Add(new(1, 2));
+        _oldItems.Add(new(5, 3));
+        _newItems.Add(new(1, 2));
+        _newItems.Add(new(5, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = _newItems, Times.Never());
+    }
+
+    [Test]
+    public void Update_OneOldOneNew()
+    {
+        var lf = new LineFold(5, 3);
+        _oldItems.Add(new(1, 2));
+        _oldItems.Add(lf);
+        _newItems.Add(new(5, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = new List<LineFold> { lf }, Times.Once());
+    }
+
+    [Test]
+    public void Update_DifferentLinesCount()
+    {
+        _oldItems.Add(new(1, 2));
+        _newItems.Add(new(1, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = _newItems, Times.Once());
+    }
+
+    [Test]
+    public void Update_AllNew()
+    {
+        _oldItems.Add(new(1, 2));
+        _oldItems.Add(new(5, 3));
+        _newItems.Add(new(10, 2));
+        _newItems.Add(new(50, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = _newItems, Times.Once());
+    }
+
+    [Test]
+    public void Update_EmptyNew()
+    {
+        _oldItems.Add(new(1, 2));
+        _oldItems.Add(new(5, 3));
+
+        Update();
+
+        _folds.VerifySet(x => x.Items = _newItems, Times.Once());
+    }
+
+    private void Update()
+    {
+        _folds.SetupGet(x => x.Items).Returns(_oldItems);
+        _updater.Update(_folds.Object, _newItems);
+    }
+}
