ad3b99a83c2edb7a2213454fa33c82b5e38c8844
Vinge Nikolay <vn2756@gmail.com>
2023-04-16 19:18:25 +0400

    HighlightBracketsRendering. Folded brackets render fix

--- CodeHighlighter.Model/Rendering/HighlightBracketsRendering.cs
+++ CodeHighlighter.Model/Rendering/HighlightBracketsRendering.cs
@@ -1,4 +1,5 @@
-﻿using CodeHighlighter.Common;
+﻿using CodeHighlighter.Ancillary;
+using CodeHighlighter.Common;
 using CodeHighlighter.Core;
 using CodeHighlighter.Model;
 using static CodeHighlighter.Ancillary.IBracketsHighlighter;
@@ -15,19 +16,25 @@ internal class HighlightBracketsRendering : IHighlightBracketsRendering
     private readonly ICodeTextBoxModel _model;
     private readonly ICodeTextBoxRenderingContext _renderingContext;
     private readonly IExtendedLineNumberGenerator _lineNumberGenerator;
+    private readonly ILineFolds _folds;
 
     public HighlightBracketsRendering(
-        ICodeTextBoxModel model, ICodeTextBoxRenderingContext renderingContext, IExtendedLineNumberGenerator lineNumberGenerator)
+        ICodeTextBoxModel model,
+        ICodeTextBoxRenderingContext renderingContext,
+        IExtendedLineNumberGenerator lineNumberGenerator,
+        ILineFolds folds)
     {
         _model = model;
         _renderingContext = renderingContext;
         _lineNumberGenerator = lineNumberGenerator;
+        _folds = folds;
     }
 
     public void Render(object platformHighlightingBrush, object platformNoPairBrush)
     {
         var brackets = _model.BracketsHighlighter.GetHighlightedBrackets(_model.CursorPosition);
         if (brackets.Kind == HighlightKind.NoHighlight) return;
+        if (_folds.IsFolded(brackets.Open.LineIndex) || _folds.IsFolded(brackets.Close.LineIndex)) return;
         if (brackets.Kind == HighlightKind.Highlighted)
         {
             _renderingContext.DrawRectangle(platformHighlightingBrush, GetBracketRect(_model, brackets.Open));
